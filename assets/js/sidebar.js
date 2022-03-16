aea426548230dbb3290251dacf5da322e962ac0d
tlm01 <napats@thalamo.com>
2022-03-16 22:08:29 +0700

    side bar update

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -18,23 +18,23 @@ $("document").ready(function () {
   setTimeout(SidebarCollapse(), 500);
 });
 
-function SidebarCollapse() {
-  $(".menu-collapsed").toggleClass("d-none");
-  $(".sidebar-submenu").toggleClass("d-none");
-  $(".submenu-icon").toggleClass("d-none");
-  $("#sidebar-container").toggleClass("sidebar-expanded sidebar-collapsed");
+// function SidebarCollapse() {
+//   $(".menu-collapsed").toggleClass("d-none");
+//   $(".sidebar-submenu").toggleClass("d-none");
+//   $(".submenu-icon").toggleClass("d-none");
+//   $("#sidebar-container").toggleClass("sidebar-expanded sidebar-collapsed");
 
-  // Treating d-flex/d-none on separators with title
-  var SeparatorTitle = $(".sidebar-separator-title");
-  if (SeparatorTitle.hasClass("d-flex")) {
-    SeparatorTitle.removeClass("d-flex");
-  } else {
-    SeparatorTitle.addClass("d-flex");
-  }
+//   // Treating d-flex/d-none on separators with title
+//   var SeparatorTitle = $(".sidebar-separator-title");
+//   if (SeparatorTitle.hasClass("d-flex")) {
+//     SeparatorTitle.removeClass("d-flex");
+//   } else {
+//     SeparatorTitle.addClass("d-flex");
+//   }
 
-  // Collapse/Expand icon
-  $("#collapse-icon").toggleClass("fa-angle-double-left fa-angle-double-right");
-}
+//   // Collapse/Expand icon
+//   $("#collapse-icon").toggleClass("fa-angle-double-left fa-angle-double-right");
+// }
 
 function SidebarCollapse() {
   $(".menu-collapsed").toggleClass("d-none");
