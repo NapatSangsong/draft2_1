7fe4e09432cdc661057c406db85236a11862cf4b
tlm01 <napats@thalamo.com>
2022-03-22 16:51:35 +0700

    page2

---
+++ assets/js/page2.js
@@ -0,0 +1,8 @@
+$("document").ready(function () {
+  $(".rightBoxContent-item-page2-collapse").click(function () {
+    $(".showDownload").fadeIn();
+  });
+  $(".closeDownload").click(function () {
+    $(".showDownload").fadeOut();
+  });
+});
