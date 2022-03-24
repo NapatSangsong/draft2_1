5e4877af9003db7de833416f686071954b52eeb2
tlm01 <napats@thalamo.com>
2022-03-24 12:12:05 +0700

    update p youy

--- assets/js/page1.js
+++ assets/js/page1.js
@@ -5,4 +5,11 @@ $("document").ready(function () {
   $(".closeDownload").click(function () {
     $(".showDownload").fadeOut();
   });
+  $("#panelbar").kendoPanelBar({
+    expandMode: "single",
+  });
+  var panelBar = $("#panelbar").data("kendoPanelBar");
+  // select the item with ID, "item1"
+  panelBar.select("#item1");
+  panelBar.expand($("#item1"));
 });
