160c3c87b7a8cf5c7d4bb37a8e9cd8c8d7a1a236
Napat Sangsong <napatsangsong@MacBook-Air.local>
2022-04-12 17:49:44 +0700

    left
    asdf

--- assets/js/gc-ci.js
+++ assets/js/gc-ci.js
@@ -1,8 +1,34 @@
 $("document").ready(function () {
   // ==== Preloader
   setTimeout(fadeout, 1000);
+  initMenu();
 });
 
 function fadeout() {
   $(".preloader").fadeOut();
+}
+
+$("#menu-toggle").click(function (e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled-2");
+  $(this).find("i").toggleClass("icon-rotate");
+});
+
+function initMenu() {
+  $("#menu ul").hide();
+  $("#menu ul").children(".current").parent().show();
+  //$('#menu ul:first').show();
+  $("#menu li a").click(function () {
+    var checkElement = $(this).next();
+    if (checkElement.is("ul") && checkElement.is(":visible")) {
+      return false;
+    }
+    if (checkElement.is("ul") && !checkElement.is(":visible")) {
+      $("#menu ul:visible").slideUp("normal");
+      checkElement.slideDown("normal");
+      return false;
+    }
+  });
+  $("#wrapper").toggleClass("toggled-2");
+  $("#menu ul").hide();
 }
